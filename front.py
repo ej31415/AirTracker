7b2d3b8e5ca215ffabe76e9dbc3753656e5a8a20
ej31415 <xiaodongxie04@gmail.com>
2023-06-26 18:29:11 -0500

    added track by flight number feature

--- front.py
+++ front.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+import tkinter.messagebox as tkmsg
 import pandas as pd
 import csv
 import back_functions
+
+initial_data = back_functions.start_program()
 
 root = tk.Tk()
 
@@ -41,16 +44,14 @@ search_choice = tk.StringVar()
 search_choice.set('---')
 search_dropdown = tk.OptionMenu(root, search_choice, *search_list)
 search_dropdown.grid(row=2, column=1)
-
-enter = tk.Entry(root, width=20)
-enter.grid(row=6, column=1, padx=1, pady=2)
-
+enterSearch = tk.Entry(root, width=20)
+enterSearch.grid(row=6, column=1, padx=1, pady=2)
 output = tk.Text(root, height=20, width=120, wrap=tk.WORD)
 output.grid(row=7, column=0, columnspan=3, padx=1, pady=2)
 
 def click_search():
     search_code = set_search()
-    user_input = enter.get()
+    user_input = enterSearch.get()
     res = "Searching for " + user_input
     lbl1.configure(text = res)
     output.delete('0.0', tk.END)
@@ -69,12 +70,38 @@ btnRestore.grid(row=1, column=2, padx=1, pady=2)
 
 def click_save():
     search_code = set_search()
-    user_input = enter.get()
+    user_input = enterSearch.get()
     data = pd.DataFrame([[user_input, search_code]])
     data.to_csv('data.csv', index=False)
     btnRestore.config(fg='green')
 
 btnSave = tk.Button(root, text="Save", command=click_save)
 btnSave.grid(row=0, column=2, padx=1, pady=2)
+
+def click_track():
+    with open("track_list.txt", 'a+') as track_list:
+        flight_num = enterTrack.get()
+        track_list.write(flight_num + "\n")
+        tkmsg.showinfo("Tracking! ", "Now tracking " + flight_num)
+    tracking.delete('0.0', tk.END)
+    tracking.insert(tk.END, back_functions.read_tracking_file())
+
+def click_clear_tracking():
+    with open("track_list.txt", "w+") as to_clear:
+        pass
+    tracking.delete('0.0', tk.END)
+    tracking.insert(tk.END, "No flights tracked")
+
+lbl2 = tk.Label(root, text="Enter flight number below to track flights")
+lbl2.grid(row=1, column=0, padx=1, pady=2)
+enterTrack = tk.Entry(root, width=20)
+enterTrack.grid(row=2, column=0, padx=1, pady=2)
+btnTrack = tk.Button(root, text="Track", command=click_track)
+btnTrack.grid(row=3, column=0, padx=1, pady=2)
+btnClear = tk.Button(root, text="Clear All", command=click_clear_tracking)
+btnClear.grid(row=4, column=0, padx=1, pady=2)
+tracking = tk.Text(root, height=10, width=120, wrap=tk.WORD)
+tracking.grid(row=8, column=0, columnspan=3, padx=1, pady=2)
+tracking.insert(tk.END, initial_data)
 
 root.mainloop()
